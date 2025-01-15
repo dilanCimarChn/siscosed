faa553f0686697b0e20b8274997074e9e1c47ac2
DilanC ChN <2002dilanchoque@gmail.com>
2025-01-15 15:05:41 -0400

    ultimas mejoras login y bienvenida

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import React from 'react';
-import BienvenidoLogin from './views/vlogin/bienvenido-login';
-import VLogin from './views/vlogin/vlogin';
-import AdminDashboard from './users/admin/AdminDashboard';
-import NormalDashboard from './users/normal/NormalDashboard';
-import SuperAdminDashboard from './users/superadmin/SuperAdminDashboard';
+import BienvenidoLogin from './views/vlogin/bienvenido-login'; // Pantalla de bienvenida
+import VLogin from './views/vlogin/vlogin'; // Pantalla de login
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NormalDashboard from './users/normal/NormalDashboard'; // Dashboard normal
+import AdminDashboard from './users/admin/AdminDashboard'; // Dashboard admin
+import SuperAdminDashboard from './users/superadmin/SuperAdminDashboard'; // Dashboard superadmin
 
-const App = () => {
-  return <VLogin/>
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Ruta para la pantalla de bienvenida */}
+        <Route path="/" element={<BienvenidoLogin />} />
+
+        {/* Ruta para el login */}
+        <Route path="/login" element={<VLogin />} />
+
+        {/* Rutas para los dashboards de los diferentes roles */}
+        <Route path="/users/normal" element={<NormalDashboard />} />
+        <Route path="/users/admin" element={<AdminDashboard />} />
+        <Route path="/users/superadmin" element={<SuperAdminDashboard />} />
+      </Routes>
+    </Router>
+  );
 }
+
 export default App;
--- frontend/src/users/superadmin/SuperAdminDashboard.jsx
+++ frontend/src/users/superadmin/SuperAdminDashboard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './superadmin.css';
 
 function SuperAdminDashboard() {
   return (
